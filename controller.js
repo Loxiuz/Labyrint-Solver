ee20b9024b88a6e60705c6a455e22acb51006807
Malte Bjoerklund <maltebjoerklund@gmail.com>
2024-11-16 00:14:36 +0100

    made maze solver into a seperate class

--- controller.js
+++ controller.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import MazeSolver from "./mazeSolver.js";
 import Stack from "./stack.js";
 
 window.addEventListener("load", async () => {
@@ -13,91 +14,22 @@ class Controller {
   constructor(boardData) {
     this.board = boardData;
     this.route = new Stack();
-    this.visited = new Stack();
   }
 
   init() {
     console.log("Controller running...");
+    this.solver();
     this.displayBoard();
-    let startCell;
-    for (let row = 0; row < this.board.rows; row++) {
-      for (let col = 0; col < this.board.cols; col++) {
-        if (row === this.board.start.row && col === this.board.start.col) {
-          startCell = this.board.maze[row][col];
-        }
-      }
-    }
-    this.visitCell(startCell);
   }
 
-  visitCell(cell) {
-    this.route.push(cell);
-    this.visited.push(cell);
-
-    console.log("Route:", this.route);
-    console.log("Visited:", this.visited);
-
-    if (cell.row === this.board.goal.row && cell.col === this.board.goal.col) {
-      console.log("Game won!");
-      console.log(this.route);
-      this.displayBoard();
-      return;
+  solver() {
+    const startCell =
+      this.board.maze[this.board.start.row][this.board.start.col];
+    const solver = new MazeSolver(this.board, startCell);
+    const route = solver.solve();
+    if (route) {
+      this.route = route;
     }
-
-    let newRow;
-    let newCol;
-
-    const directions = ["north", "east", "west", "south"];
-
-    for (const direction of directions) {
-      switch (direction) {
-        case "north":
-          newRow = cell.row - 1;
-          newCol = cell.col + 0;
-          break;
-        case "east":
-          newRow = cell.row + 0;
-          newCol = cell.col + 1;
-          break;
-        case "west":
-          newRow = cell.row + 0;
-          newCol = cell.col - 1;
-          break;
-        case "south":
-          newRow = cell.row + 1;
-          newCol = cell.col + 0;
-          break;
-      }
-
-      if (
-        newRow >= 0 &&
-        newRow < this.board.rows &&
-        newCol >= 0 &&
-        newCol < this.board.cols
-      ) {
-        const nextCell = this.board.maze[newRow][newCol];
-
-        if (!this.hasVisited(nextCell)) {
-          if (!cell[direction]) {
-            console.log("Going:", direction);
-            this.visitCell(nextCell);
-          }
-        }
-      }
-    }
-    console.log("Backtracking...");
-    this.route.pop();
-    return false;
-  }
-
-  hasVisited(cell) {
-    for (let i = 0; i < this.visited.size(); i++) {
-      const visited = this.visited.get(i);
-      if (visited.row === cell.row && visited.col === cell.col) {
-        return true;
-      }
-    }
-    return false;
   }
 
   displayBoard() {
@@ -138,10 +70,12 @@ class Controller {
         ) {
           cellElement.classList.add("goal");
         }
-        for (let i = 0; i < this.route.size(); i++) {
-          const routeCell = this.route.get(i);
-          if (routeCell.row === cell.row && routeCell.col === cell.col) {
-            cellElement.innerHTML = `<div class="route"></div>`;
+        if (this.route) {
+          for (let i = 0; i < this.route.size(); i++) {
+            const routeCell = this.route.get(i);
+            if (routeCell.row === cell.row && routeCell.col === cell.col) {
+              cellElement.innerHTML = `<div class="route"></div>`;
+            }
           }
         }
       }
